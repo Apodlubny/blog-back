e16b44c4c4cab3b6bb686323c3fb16ca980084b4
Apodlubny <apodlubny@gmail.com>
2023-05-06 14:30:07 +0300

    jwt connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 //мідлвара для читання запросів з клієнта
@@ -10,7 +11,14 @@ app.get("/", (req, res) => {
 
 app.post("auth/login", (req, res) => {
   console.log(req.body);
-  res.json({ success: true });
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Ім'я користувача",
+    },
+    "secretKey"
+  );
+  res.json({ success: true, token });
 });
 
 app.listen(4444, (err) => {
