ef56d45f4e985bbbf2a5e29781f16d887e49be8c
Apodlubny <apodlubny@gmail.com>
2023-05-06 15:08:58 +0300

    mongoose connect

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-
+import mongoose from "mongoose";
+mongoose.connect(
+  "mongodb+srv://<username>:<password>@cluster0.ics529r.mongodb.net/?retryWrites=true&w=majority"
+);
 const app = express();
 //мідлвара для читання запросів з клієнта
 app.use(express.json());
@@ -9,8 +12,9 @@ app.get("/", (req, res) => {
   res.send("Works!");
 });
 
-app.post("auth/login", (req, res) => {
+app.post("/auth/login", (req, res) => {
   console.log(req.body);
+  //генеруємо токен
   const token = jwt.sign(
     {
       email: req.body.email,
